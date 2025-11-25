d7835827bac5458f48a48cdb6f2bc8da12b1be87
Salem Mohammed Shamakh <salem.m.s2030@gmail.com>
2025-11-25 17:07:23 +0300

    Lecture 9: Navigation - Template

--- lib/main.dart
+++ lib/main.dart
@@ -10,34 +10,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Lecture 8 - Handling User Input',
+      title: 'Lecture 9 - Navigation',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const UserInputPage(),
+      home: const NavigationPage(),
     );
   }
 }
 
-class UserInputPage extends StatefulWidget {
-  const UserInputPage({super.key});
+class NavigationPage extends StatelessWidget {
+  const NavigationPage({super.key});
 
-  @override
-  State<UserInputPage> createState() => _UserInputPageState();
-}
-
-class _UserInputPageState extends State<UserInputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Handling User Input'),
+        title: const Text('Navigation'),
       ),
       body: const Center(
         child: Text(
-          'Lecture 8 - Handling User Input',
+          'Lecture 9 - Navigation',
           style: TextStyle(fontSize: 20),
         ),
       ),
