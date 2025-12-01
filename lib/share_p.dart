066ac0773783740dce05a8e898e7f36181f63afd
Salem Mohammed Shamakh <salem.m.s2030@gmail.com>
2025-12-01 10:26:29 +0300

    .

---
+++ lib/share_p.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import "shaew"
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Course',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(
+        body: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              children: [
+                Text(
+                  'Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects ',
+                ),
+                Text(
+                  'Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects ',
+                ),
+                Text(
+                  'Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects ',
+                ),
+                Text(
+                  'Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects ',
+                ),
+                Text(
+                  'Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects ',
+                ),
+                Text(
+                  'Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects ',
+                ),
+                Text(
+                  'Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects ',
+                ),
+                Text(
+                  'Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects ',
+                ),
+                Text(
+                  'Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects ',
+                ),
+                Text(
+                  'Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects ',
+                ),
+                Text(
+                  'Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects ',
+                ),
+                Text(
+                  'Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects ',
+                ),
+                Text(
+                  'Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects ',
+                ),
+                Text(
+                  'Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects ',
+                ),
+                Text(
+                  'Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects ',
+                ),
+                Text(
+                  'Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects ',
+                ),
+                Text(
+                  'Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects ',
+                ),
+                Text(
+                  'Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects ',
+                ),
+                Text(
+                  'Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects ',
+                ),
+                Text(
+                  'Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects ',
+                ),
+                Text(
+                  'Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects ',
+                ),
+                Text(
+                  'Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects ',
+                ),
+                Text(
+                  'Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects ',
+                ),
+                Text(
+                  'Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects ',
+                ),
+                Text(
+                  'Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects ',
+                ),
+                Text(
+                  'Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects Explore branches to see the projects ',
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
